da229014dc41026601cb66ca1fd7b6f1351db2ba
Jax <ngjiaxun@gmail.com>
2023-12-28 21:15:07 +0800

    Remove BranchAssignment, BusinessAssignment, and OrganisationAssignment models

--- multiuser/admin.py
+++ multiuser/admin.py
@@ -2,13 +2,6 @@ from django.contrib import admin
 from .models import *
 from guardian.admin import GuardedModelAdmin
 
-# admin.site.register(Organisation)
-# admin.site.register(Business)
-# admin.site.register(Branch)
-# admin.site.register(OrganisationAssignment)
-# admin.site.register(BusinessAssignment)
-# admin.site.register(BranchAssignment)
-
 @admin.register(Organisation)
 class OrganisationAdmin(GuardedModelAdmin):
     pass
--- multiuser/models.py
+++ multiuser/models.py
@@ -7,28 +7,12 @@ class Organisation(models.Model):
     address = models.CharField(max_length=200)
     contact_number = models.CharField(max_length=20)
     email = models.EmailField()
-    users = models.ManyToManyField(User, through='OrganisationAssignment')
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse("organisation_detail", kwargs={"pk": self.pk})
-    
-
-class OrganisationAssignment(models.Model):
-    ROLE_ADMIN = 'admin'
-    ROLE_USER = 'user'
-    ROLE_CHOICES = (
-        (ROLE_ADMIN, 'Admin'),
-        (ROLE_USER, 'User'),
-    )
-    organisation = models.ForeignKey(Organisation, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=100, choices=ROLE_CHOICES)
-
-    def __str__(self):
-        return f"{self.organisation.name} - {self.user.username}"
 
 
 class Business(models.Model):
@@ -44,21 +28,6 @@ class Business(models.Model):
         return reverse("business_detail", kwargs={"pk": self.pk})
 
 
-class BusinessAssignment(models.Model):
-    ROLE_ADMIN = 'admin'
-    ROLE_USER = 'user'
-    ROLE_CHOICES = (
-        (ROLE_ADMIN, 'Admin'),
-        (ROLE_USER, 'User'),
-    )
-    business = models.ForeignKey(Business, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=100, choices=ROLE_CHOICES)
-
-    def __str__(self):
-        return f"{self.business.name} - {self.user.username}"
-
-
 class Branch(models.Model):
     name = models.CharField(max_length=100)
     business = models.ForeignKey(Business, on_delete=models.CASCADE)
@@ -68,18 +37,3 @@ class Branch(models.Model):
 
     def __str__(self):
         return self.name
-
-
-class BranchAssignment(models.Model):
-    ROLE_ADMIN = 'admin'
-    ROLE_USER = 'user'
-    ROLE_CHOICES = (
-        (ROLE_ADMIN, 'Admin'),
-        (ROLE_USER, 'User'),
-    )
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=100, choices=ROLE_CHOICES)
-
-    def __str__(self):
-        return f"{self.branch.name} - {self.user.username}"
