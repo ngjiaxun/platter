ee62788c49c9200bbad30d0cb9a9252ee0c8f98a
Jax <ngjiaxun@gmail.com>
2023-12-28 18:25:32 +0800

    Add Django Guardian

--- multiuser/admin.py
+++ multiuser/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
-from django.contrib import admin
 from .models import *
+from guardian.admin import GuardedModelAdmin
 
-admin.site.register(Organisation)
-admin.site.register(Business)
-admin.site.register(Branch)
-admin.site.register(OrganisationAssignment)
-admin.site.register(BusinessAssignment)
-admin.site.register(BranchAssignment)
+# admin.site.register(Organisation)
+# admin.site.register(Business)
+# admin.site.register(Branch)
+# admin.site.register(OrganisationAssignment)
+# admin.site.register(BusinessAssignment)
+# admin.site.register(BranchAssignment)
+
+@admin.register(Organisation)
+class OrganisationAdmin(GuardedModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Business)
+class BusinessAdmin(GuardedModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Branch)
+class BranchAdmin(GuardedModelAdmin):
+    list_display = ('name', )
--- plattersystems/settings.py
+++ plattersystems/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'guardian',
     'multiuser',
 ]
 
@@ -124,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = (
+    "django.contrib.auth.backends.ModelBackend",
+    "guardian.backends.ObjectPermissionBackend",
+)
