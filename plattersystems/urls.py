89b5aea577247b416153e85b2b043cf0d61a9982
Jax <ngjiaxun@gmail.com>
2023-12-29 12:49:33 +0800

    Add registration feature

--- plattersystems/urls.py
+++ plattersystems/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include("django.contrib.auth.urls")),
+    path('sign-up', views.sign_up, name='sign_up'),
     path('multiuser/', include('multiuser.urls')),
 ]
---
+++ plattersystems/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+
+# def home(request):
+#     return render(request, 'multiuser/templates/organisation_list.html')
+
+def sign_up(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user, backend='django.contrib.auth.backends.ModelBackend')
+            return redirect('/multiuser/organisation/')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/sign_up.html', {"form": form})
