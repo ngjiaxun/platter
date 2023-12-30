1144cd6748b32e88b24284f5978c75054b030069
Jax <ngjiaxun@gmail.com>
2023-12-30 14:17:51 +0800

    Make dynamically created object permission group names unique by adding their pk

--- multiuser/signals.py
+++ multiuser/signals.py
@@ -1,20 +1,20 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, m2m_changed
 from django.dispatch import receiver
-from django.contrib.auth.models import Group, Permission
+from django.contrib.auth.models import Group, Permission, User
 from guardian.shortcuts import assign_perm
 from .models import Organisation, Business, Branch
 
 def create_groups(instance, created):
     if created:
         # Create the admin group
-        admin_group, created = Group.objects.get_or_create(name=f'{instance.name}_{instance._meta.model_name}_admins')
+        admin_group, created = Group.objects.get_or_create(name=f'{instance.name}{instance.pk}_{instance._meta.model_name}_admins')
         # Assign all permissions for the instance to the admin group
         assign_perm(f'view_{instance._meta.model_name}', admin_group, instance)
         assign_perm(f'change_{instance._meta.model_name}', admin_group, instance)
         assign_perm(f'delete_{instance._meta.model_name}', admin_group, instance)
 
         # Create the user group
-        user_group, created = Group.objects.get_or_create(name=f'{instance.name}_{instance._meta.model_name}_users')
+        user_group, created = Group.objects.get_or_create(name=f'{instance.name}{instance.pk}_{instance._meta.model_name}_users')
         # Assign the view permission for the instance to the user group
         assign_perm(f'view_{instance._meta.model_name}', user_group, instance)
 
@@ -22,9 +22,7 @@ def create_groups(instance, created):
         instance.created_by.groups.add(admin_group)
 
 def delete_groups(instance):
-    # Delete the admin group
     Group.objects.get(name=f'{instance.name}_{instance._meta.model_name}_admins').delete()
-    # Delete the user group
     Group.objects.get(name=f'{instance.name}_{instance._meta.model_name}_users').delete()
 
 @receiver(post_save, sender=Organisation)
@@ -50,3 +48,4 @@ def create_branch_groups(sender, instance, created, **kwargs):
 @receiver(post_delete, sender=Branch)
 def delete_branch_groups(sender, instance, **kwargs):
     delete_groups(instance)
+
